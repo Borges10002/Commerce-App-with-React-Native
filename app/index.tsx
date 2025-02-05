25655fe016b5222c074f4f86709c3cc7c2dc6b9a
Borges10002 <borges10002@gmail.com>
2025-02-04 21:05:41 -0400

    first projet in git

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="font-bold">
+      <Text className="text-red-500">Raghunadh</Text>
+    </View>
+  );
+}
