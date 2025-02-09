8baa1dbdad8b48bc82024aeec4795594af318148
Borges10002 <borges10002@gmail.com>
2025-02-09 09:12:31 -0400

    fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,29 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        rubik: ["Rubik-Regular", "sans-serif"],
+        "rubik-bold": ["Rubik-Bold", "sans-serif"],
+        "rubik-extrabold": ["Rubik-ExtraBold", "sans-serif"],
+        "rubik-medium": ["Rubik-Medium", "sans-serif"],
+        "rubik-light": ["Rubik-Light", "sans-serif"],
+        "rubik-semibold": ["Rubik-SemiBold", "sans-serif"],
+      },
+      colors: {
+        primary: {
+          50: "#E2E0E0",
+          100: "#8E6CEF",
+        },
+      },
+      gray: "#F4F4F4",
+      black: {
+        DEFAULT: "#000",
+        100: "#272727",
+        50: "#858484",
+      },
+      danger: "#F75555",
+    },
   },
   plugins: [],
 };
